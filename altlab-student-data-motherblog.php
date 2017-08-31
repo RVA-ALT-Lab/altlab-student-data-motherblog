8c70740eb2a1e063485dca6658ba4ad955831521
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-08-31 08:50:59 -0400

    update to add some functionality

--- altlab-student-data-motherblog.php
+++ altlab-student-data-motherblog.php
@@ -9,10 +9,6 @@
 */
 
 
-
-
-
-
 function altlab_student_data_motherblog_menu(){
 	add_menu_page(
 		'Mother Blog Student Data', 
@@ -34,10 +30,39 @@ function altlab_student_data_motherblog_options_page(){
 
 	}
 
-	
+	require_once(plugin_dir_path(__FILE__) . '/controllers/collect-student-data.php'); 
+	require_once(plugin_dir_path(__FILE__) . '/views/student-data.php'); 
 
 
 }
 
 add_action('admin_menu', 'altlab_student_data_motherblog_menu'); 
+
+
+
+
+function altlab_student_data_motherblog_shortcode($atts = [], $content = null){
+		
+		ob_start(); 
+
+
+		require_once(plugin_dir_path(__FILE__) . '/controllers/collect-student-data.php'); 
+		require_once(plugin_dir_path(__FILE__) . '/views/student-data.php'); 
+
+		$output = ob_get_clean(); 
+		$run_shortcodes = do_shortcode($output); 
+		return $run_shortcodes;  
+}
+
+
+// function init_shortcodes(){
+// 	add_shortcode('altlabstudentdatamotherblog', 'altlab_student_data_motherblog_shortcode'); 
+// }
+
+
+
 ?>
+
+
+
+
---
+++ controllers/collect-student-data.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+	  $authors = [];
+      $args = array(
+        'blog_id'      => $GLOBALS['blog_id'],
+        'role'         => 'author',
+        'orderby' => 'display_name',
+
+       ); 
+       $the_users = get_users( $args ); 
+       foreach ( $the_users as $author ) {
+         $authors[$author->user_email] = array(
+            'userEmail' => $author->user_email, 
+
+          ); 
+       }
+
+
+
+?>
